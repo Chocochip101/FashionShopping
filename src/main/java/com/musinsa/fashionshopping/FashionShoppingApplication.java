af0a4d85d440350839f45d015a871223b21c65db
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-02-28 17:41:15 +0900

    init: 프로젝트 셋팅

---
+++ src/main/java/com/musinsa/fashionshopping/FashionShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.musinsa.fashionshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionShoppingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FashionShoppingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/musinsa/fashionshopping/FashionShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musinsa.fashionshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionShoppingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
