93e5552e16ce03e9a9b2db545a3bca55d632e6e9
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-02-29 12:02:15 +0900

    feat: RestDocs 테스트 변경

---
+++ src/test/java/com/musinsa/fashionshopping/ControllerTest.java
@@ -0,0 +1,40 @@
+package com.musinsa.fashionshopping;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+import com.musinsa.fashionshopping.brand.controller.BrandController;
+import com.musinsa.fashionshopping.brand.service.BrandService;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import io.restassured.module.mockmvc.specification.MockMvcRequestSpecification;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@WebMvcTest({
+        BrandController.class
+})
+@ExtendWith(RestDocumentationExtension.class)
+public class ControllerTest {
+    protected MockMvcRequestSpecification restDocs;
+    @MockBean
+    protected BrandService brandService;
+
+    @BeforeEach
+    void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
+        restDocs = RestAssuredMockMvc.given()
+                .mockMvc(MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                        .apply(documentationConfiguration(restDocumentation)
+                                .operationPreprocessors()
+                                .withRequestDefaults(prettyPrint())
+                                .withResponseDefaults(prettyPrint()))
+                        .build())
+                .log().all();
+    }
+}
+
--- src/test/java/com/musinsa/fashionshopping/brand/controller/BrandControllerTest.java
+++ src/test/java/com/musinsa/fashionshopping/brand/controller/BrandControllerTest.java
@@ -3,43 +3,27 @@ package com.musinsa.fashionshopping.brand.controller;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.musinsa.fashionshopping.ControllerTest;
 import com.musinsa.fashionshopping.brand.controller.dto.NewBrandRequest;
 import com.musinsa.fashionshopping.brand.exception.DuplicateBrandNameException;
 import com.musinsa.fashionshopping.brand.exception.InvalidBrandNameException;
-import com.musinsa.fashionshopping.brand.service.BrandService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-class BrandControllerTest {
-    @Autowired
-    private MockMvc mockMvc;
-    @Autowired
-    private ObjectMapper objectMapper;
-    @MockBean
-    private BrandService brandService;
+class BrandControllerTest extends ControllerTest {
 
     @DisplayName("브랜드를 생성하면 201을 반환한다.")
     @Test
-    void createBrand() throws Exception {
+    void createBrand() {
         //given
         NewBrandRequest newBrandRequest = new NewBrandRequest("nike");
-        String brandRequestJson = objectMapper.writeValueAsString(newBrandRequest);
 
         //when
         doNothing()
@@ -47,52 +31,54 @@ class BrandControllerTest {
                 .createBrand(newBrandRequest);
 
         //then
-        mockMvc.perform(
-                        post("/brands")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(brandRequestJson))
-                .andExpect(status().isCreated())
-                .andDo(print());
+        restDocs
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(newBrandRequest)
+                .when().post("/brands")
+                .then().log().all()
+                .apply(document("brands/create/success"))
+                .statusCode(HttpStatus.CREATED.value());
     }
+
 
     @DisplayName("브랜드 생성 요청에 빈 이름에 대해 400을 반환한다.")
     @ParameterizedTest
     @NullSource
     @ValueSource(strings = {"", "  "})
-    void createBrand_Exception_NoName(final String name) throws Exception {
+    void createBrand_Exception_NoName(final String name) {
         //given
         NewBrandRequest newBrandRequest = new NewBrandRequest(name);
-        String brandRequestJson = objectMapper.writeValueAsString(newBrandRequest);
 
         //then
-        mockMvc.perform(
-                        post("/brands")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(brandRequestJson))
-                .andExpect(status().isBadRequest())
-                .andDo(print());
+        restDocs
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(newBrandRequest)
+                .when().post("/brands")
+                .then().log().all()
+                .apply(document("brands/create/fail"))
+                .statusCode(HttpStatus.BAD_REQUEST.value());
     }
 
     @DisplayName("브랜드 이름 등록 시 잘못된 형식이면 400 반환한다.")
     @ParameterizedTest
     @ValueSource(strings = {"ㄱㄴㄷ", "abdf123ㅏㅇ", "11112222333344445"})
-    void createBrand_Exception_InvalidFormat(final String invalidBrandName) throws Exception {
+    void createBrand_Exception_InvalidFormat(final String invalidBrandName) {
         //given
         NewBrandRequest newBrandRequest = new NewBrandRequest(invalidBrandName);
-        String brandRequestJson = objectMapper.writeValueAsString(newBrandRequest);
 
         //when
-        doThrow(InvalidBrandNameException.class)
+        doThrow(new InvalidBrandNameException())
                 .when(brandService)
                 .createBrand(any());
 
         //when & then
-        mockMvc.perform(
-                        post("/brands")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(brandRequestJson))
-                .andExpect(status().isBadRequest())
-                .andDo(print());
+        restDocs
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(newBrandRequest)
+                .when().post("/brands")
+                .then().log().all()
+                .apply(document("brands/create/fail/invalidFormat"))
+                .statusCode(HttpStatus.BAD_REQUEST.value());
     }
 
     @DisplayName("중복된 브랜드 이름 등록 시 400 반환한다.")
@@ -100,19 +86,19 @@ class BrandControllerTest {
     void createBrand_Exception_Duplicated() throws Exception {
         //given
         NewBrandRequest newBrandRequest = new NewBrandRequest("nike");
-        String brandRequestJson = objectMapper.writeValueAsString(newBrandRequest);
 
         //when
-        doThrow(DuplicateBrandNameException.class)
+        doThrow(new DuplicateBrandNameException())
                 .when(brandService)
                 .createBrand(any());
 
-        //when & then
-        mockMvc.perform(
-                        post("/brands")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(brandRequestJson))
-                .andExpect(status().isBadRequest())
-                .andDo(print());
+        //then
+        restDocs
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(newBrandRequest)
+                .when().post("/brands")
+                .then().log().all()
+                .apply(document("brands/create/fail/duplicateBrandName"))
+                .statusCode(HttpStatus.BAD_REQUEST.value());
     }
 }
