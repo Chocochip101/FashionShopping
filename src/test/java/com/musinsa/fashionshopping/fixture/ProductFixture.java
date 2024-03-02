c678d010b0777562c4d3e6c2300021a2ab09a1d5
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-03-02 11:57:51 +0900

    test: 상품 fixture 생성

---
+++ src/test/java/com/musinsa/fashionshopping/fixture/ProductFixture.java
@@ -0,0 +1,30 @@
+package com.musinsa.fashionshopping.fixture;
+
+import static com.musinsa.fashionshopping.fixture.BrandFixture.createBrandA;
+import static com.musinsa.fashionshopping.fixture.BrandFixture.createBrandB;
+
+import com.musinsa.fashionshopping.product.domain.Category;
+import com.musinsa.fashionshopping.product.domain.Product;
+import com.musinsa.fashionshopping.product.domain.ProductPrice;
+
+public class ProductFixture {
+    private ProductFixture() {
+    }
+
+    public static Product createProductATop() {
+        return Product.builder()
+                .productPrice(new ProductPrice(11_200L))
+                .category(Category.TOP)
+                .brand(createBrandA())
+                .build();
+
+    }
+
+    public static Product createProductBTop() {
+        return Product.builder()
+                .productPrice(new ProductPrice(10_500L))
+                .category(Category.TOP)
+                .brand(createBrandB())
+                .build();
+    }
+}
